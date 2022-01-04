57ed80f8e78746aa3210976200461f254bee858e
AbdullohFlutterDeveloper <abdullohabduxalilov21982@gmail.com>
2022-01-04 16:19:56 +0500

    First Upload

---
+++ meditation_app/lib/screens/main_screen/main_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meditation_app/constants/size_config.dart';
+import 'package:meditation_app/provider/profile_provider.dart';
+import 'package:meditation_app/screens/sound_screen/sound_screen.dart';
+import 'package:meditation_app/widget/my_appbar.dart';
+import 'package:provider/provider.dart';
+import 'package:meditation_app/widget/bottom_nav_bar.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: (context.watch<ProfileProvider>().selectedIndex == 2)
+          ? MyAppBar(title: "edit")
+          : MyAppBar(title: "nothitng"),
+      body: (context.watch<ProfileProvider>().selectedIndex == 1 &&
+              context.watch<ProfileProvider>().page == 2)
+          ? SoundScreen()
+          : context.read<ProfileProvider>().bodies(),
+      bottomNavigationBar: bottomNavbar(context),
+    );
+  }
+}
---
+++ meditation_app/lib/widget/bottom_nav_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meditation_app/provider/profile_provider.dart';
+import 'package:provider/provider.dart';
+bottomNavbar(BuildContext context){
+  return BottomNavigationBar(
+    onTap: (e) {
+      Provider.of<ProfileProvider>(context, listen: false).selectedIndex = e;
+      Provider.of<ProfileProvider>(context, listen: false).notify();
+
+    },
+    currentIndex: context.watch<ProfileProvider>().selectedIndex,
+    items: const [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: "",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.music_note),
+        label: "",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: "",
+      ),
+    ],
+  );
+}
